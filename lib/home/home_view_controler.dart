65fe4dc1d825e76b22656f54f6bdf8d6da1968b4
PeliconPH <peliconp.h@gmail.com>
2023-01-21 11:42:00 -0400

    nome e IMDB

--- lib/home/home_view_controler.dart
+++ lib/home/home_view_controler.dart
@@ -42,11 +42,72 @@ class _HomeViewControlerState extends State<HomeViewControler> {
                           scrollDirection: Axis.horizontal,
                           itemCount: widget.viewModel.movies.length,
                           itemBuilder: ((_, index) {
+                            final rate = widget.viewModel.movies[index].voteAverage;
+                            final titleMovies = widget.viewModel.movies[index].title;
                             return Padding(
-                              padding: const EdgeInsets.all(16),
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(12),
-                                child: Image.network(widget.viewModel.movies[index].image),
+                              padding: const EdgeInsets.all(12),
+                              child: Stack(
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(12),
+                                    child: Image.network(widget.viewModel.movies[index].image),
+                                  ),
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Positioned(
+                                        bottom: 16,
+                                        left: 16,
+                                        child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(12),
+                                          child: Stack(
+                                            alignment: AlignmentDirectional.center,
+                                            children: [
+                                              Container(
+                                                color: Colors.green,
+                                                width: 100,
+                                                height: 40,
+                                              ),
+                                              Text(
+                                                'IMDB $rate',
+                                                textAlign: TextAlign.justify,
+                                                overflow: TextOverflow.ellipsis,
+                                                style: const TextStyle(fontWeight: FontWeight.bold),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                      Positioned(
+                                        bottom: 16,
+                                        left: 16,
+                                        child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(12),
+                                          child: Stack(
+                                            alignment: AlignmentDirectional.center,
+                                            children: [
+                                              Container(
+                                                color: Colors.black,
+                                                width: 185,
+                                                height: 60,
+                                              ),
+                                              SizedBox(
+                                                width: 185,
+                                                child: Text(
+                                                  titleMovies,
+                                                  textAlign: TextAlign.justify,
+                                                  overflow: TextOverflow.ellipsis,
+                                                  style: const TextStyle(fontWeight: FontWeight.bold),
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
                               ),
                             );
                           }),
@@ -57,11 +118,72 @@ class _HomeViewControlerState extends State<HomeViewControler> {
                           scrollDirection: Axis.horizontal,
                           itemCount: widget.viewModel.topMovies.length,
                           itemBuilder: ((_, index) {
+                            final rateTop = widget.viewModel.topMovies[index].voteAverage;
+                            final titleTopMovies = widget.viewModel.topMovies[index].title;
                             return Padding(
-                              padding: const EdgeInsets.all(16),
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(12),
-                                child: Image.network(widget.viewModel.topMovies[index].image),
+                              padding: const EdgeInsets.all(12),
+                              child: Stack(
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(12),
+                                    child: Image.network(widget.viewModel.topMovies[index].image),
+                                  ),
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Positioned(
+                                        bottom: 16,
+                                        left: 16,
+                                        child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(12),
+                                          child: Stack(
+                                            alignment: AlignmentDirectional.center,
+                                            children: [
+                                              Container(
+                                                color: Colors.green,
+                                                width: 100,
+                                                height: 40,
+                                              ),
+                                              Text(
+                                                'IMDB $rateTop',
+                                                textAlign: TextAlign.justify,
+                                                overflow: TextOverflow.ellipsis,
+                                                style: const TextStyle(fontWeight: FontWeight.bold),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                      Positioned(
+                                        bottom: 16,
+                                        left: 16,
+                                        child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(12),
+                                          child: Stack(
+                                            alignment: AlignmentDirectional.center,
+                                            children: [
+                                              Container(
+                                                color: Colors.black,
+                                                width: 185,
+                                                height: 60,
+                                              ),
+                                              SizedBox(
+                                                width: 185,
+                                                child: Text(
+                                                  titleTopMovies,
+                                                  textAlign: TextAlign.justify,
+                                                  overflow: TextOverflow.ellipsis,
+                                                  style: const TextStyle(fontWeight: FontWeight.bold),
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
                               ),
                             );
                           }),
